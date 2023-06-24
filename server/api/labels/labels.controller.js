cd2b5948527534d037eb8c38741aa9321ec4a8a4
RTANC <34498692+RTANC@users.noreply.github.com>
2023-06-24 14:51:41 +0700

    fix update label api

--- server/api/labels/labels.controller.js
+++ server/api/labels/labels.controller.js
@@ -9,7 +9,7 @@ exports.label = async (req, res, next) => {
         }
 
         let sql_query = `UPDATE ${tblName}
-        SET t_label1 = ${req.body.label1 || 'NULL'}, t_label2 = ${req.body.label2 || 'NULL'}, t_label3 = ${req.body.label3 || 'NULL'}
+        SET t_label1 = ${(req.body.label1 !== '') ? req.body.label1 : 'NULL'}, t_label2 = ${(req.body.label2 !== '') ? req.body.label2 : 'NULL'}, t_label3 = ${(req.body.label3 !== '') ? req.body.label3 : 'NULL'}
         WHERE t_id = ${req.params.id}`
 
         await sequelize.query(sql_query, { type: QueryTypes.UPDATE })
